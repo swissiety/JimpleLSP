0189678e90aafc4deaa24ca594a9abbf1fdfa8b0
Markus Schmidt <markus262@web.de>
2020-10-19 13:04:35 +0200

    add scaffold for mutable project/view

---
+++ src/main/java/com/github/swissiety/jimplelsp/soot/MutableProject.java
@@ -0,0 +1,91 @@
+package com.github.swissiety.jimplelsp.soot;
+
+import de.upb.swt.soot.core.*;
+import de.upb.swt.soot.core.frontend.SootClassSource;
+import de.upb.swt.soot.core.inputlocation.AnalysisInputLocation;
+import de.upb.swt.soot.core.inputlocation.ClassLoadingOptions;
+import de.upb.swt.soot.core.inputlocation.EagerInputLocation;
+import de.upb.swt.soot.core.views.View;
+
+import javax.annotation.Nonnull;
+import java.util.*;
+import java.util.function.Function;
+
+/**
+ * author: Markus Schmidt
+ */
+public class MutableProject<P extends Project, V extends View> extends Project {
+
+  private final P project;
+  private static final List<AnalysisInputLocation> inputLocation = new ArrayList<>();
+
+  public MutableProject(P project) {
+    // FIXME: we need to provide a reference to this class to forward getInputLocations to here..
+    super(project.getLanguage(), Collections.emptyList(), project.getIdentifierFactory(), project.getSourceTypeSpecifier());
+    this.project = project;
+
+    /*
+    TODO: We need to restructure Soots: Project/Subclasses -> returntype: generic <V extends View>
+    TODO: Listener to have an com.github.swissiety.jimplelsp.soot.MutableView, too (which wraps an instance of <V> )
+
+    final Class<? extends View> c = View.class;
+    Constructor<?> cons = null;
+    try {
+      cons = c.getConstructor(JavaView.class);
+      View view = (View) cons.newInstance( project );
+
+    } catch (NoSuchMethodException | IllegalAccessException | InstantiationException | InvocationTargetException e) {
+      e.printStackTrace();
+    }
+    */
+  }
+
+  void addClassSource(SootClassSource cs) {
+    addInputLocation(new EagerInputLocation(Collections.singletonMap(cs.getClassType(), cs)));
+  }
+
+  void addInputLocation(AnalysisInputLocation ana) {
+    final boolean added = inputLocation.add(ana);
+    if (added) {
+      // TODO: fire events?
+    }
+  }
+
+  void removeInputLocation(AnalysisInputLocation ana) {
+    final boolean removed = inputLocation.remove(ana);
+    if (removed) {
+      // TODO: fire events?
+    }
+  }
+
+  @Override
+  @Nonnull
+  public List<AnalysisInputLocation> getInputLocations() {
+    return inputLocation;
+  }
+
+  @Override
+  @Nonnull
+  public View createFullView() {
+    return new MutableView<>(project, project.createFullView());
+  }
+
+  @Override
+  @Nonnull
+  public View createOnDemandView() {
+    return new MutableView<>(project, project.createOnDemandView());
+  }
+
+  @Override
+  @Nonnull
+  public View createOnDemandView(Function<AnalysisInputLocation, ClassLoadingOptions> function) {
+    return new MutableView<>(project, project.createOnDemandView(function));
+  }
+
+  @Override
+  @Nonnull
+  public View createView(Scope scope) {
+    return new MutableView<>(project, project.createView(scope));
+  }
+
+}
---
+++ src/main/java/com/github/swissiety/jimplelsp/soot/ViewChangeListener.java
@@ -0,0 +1,19 @@
+package com.github.swissiety.jimplelsp.soot;
+
+import de.upb.swt.soot.core.model.SootClass;
+import de.upb.swt.soot.core.model.SootMethod;
+
+public interface ViewChangeListener {
+  void classAdded(SootClass sc);
+
+  void classChanged(SootClass oldClass, SootClass newClass);
+
+  void classRemoved(SootClass sc);
+
+  void methodAdded(SootMethod m);
+
+  void methodChanged(SootMethod oldMethod, SootMethod newMethod);
+
+  void methodRemoved(SootMethod m);
+
+}
