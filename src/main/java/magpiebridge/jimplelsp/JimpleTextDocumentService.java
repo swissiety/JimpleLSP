3d9d88cf78c27a68e54688e3e337d79b89e8c5c3
Markus Schmidt <markus262@web.de>
2020-10-25 23:35:01 +0100

    implement references() / find usages

--- src/main/java/magpiebridge/jimplelsp/JimpleTextDocumentService.java
+++ src/main/java/magpiebridge/jimplelsp/JimpleTextDocumentService.java
@@ -2,6 +2,13 @@ package magpiebridge.jimplelsp;
 
 import de.upb.swt.soot.core.frontend.AbstractClassSource;
 import de.upb.swt.soot.core.frontend.ResolveException;
+import de.upb.swt.soot.core.jimple.basic.Value;
+import de.upb.swt.soot.core.jimple.common.expr.AbstractInvokeExpr;
+import de.upb.swt.soot.core.jimple.common.expr.JStaticInvokeExpr;
+import de.upb.swt.soot.core.jimple.common.ref.JFieldRef;
+import de.upb.swt.soot.core.jimple.common.stmt.JAssignStmt;
+import de.upb.swt.soot.core.jimple.common.stmt.JInvokeStmt;
+import de.upb.swt.soot.core.jimple.common.stmt.Stmt;
 import de.upb.swt.soot.core.model.*;
 import de.upb.swt.soot.core.signatures.FieldSignature;
 import de.upb.swt.soot.core.signatures.MethodSignature;
@@ -16,7 +23,9 @@ import magpiebridge.jimplelsp.provider.JimpleLabelLinkProvider;
 import org.antlr.v4.runtime.*;
 import org.eclipse.lsp4j.*;
 import org.eclipse.lsp4j.jsonrpc.messages.Either;
+import soot.jimple.Constant;
 
+import javax.annotation.Nonnull;
 import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.*;
@@ -26,35 +35,59 @@ import java.util.concurrent.CompletableFuture;
  * @author Markus Schmidt
  */
 public class JimpleTextDocumentService extends MagpieTextDocumentService {
-  private Map<String, SignaturePositionResolver> docPosResolver = new HashMap<>();
+  private final Map<String, SignaturePositionResolver> docSignaturePositionResolver = new HashMap<>();
 
   /**
    * Instantiates a new magpie text document service.
    *
    * @param server the server
    */
-  public JimpleTextDocumentService(MagpieServer server) {
+  public JimpleTextDocumentService(@Nonnull MagpieServer server) {
     super(server);
   }
 
+  @Nonnull
   JimpleLspServer getServer() {
     return (JimpleLspServer) server;
+  }
+
+  @Nonnull
+  JimpleParser createParserForUri(String fileUri) throws IOException {
+    JimpleLexer lexer = new JimpleLexer(CharStreams.fromPath(Paths.get(fileUri)));
+    TokenStream tokens = new CommonTokenStream(lexer);
+    JimpleParser parser = new JimpleParser(tokens);
+    parser.removeErrorListeners();
+    parser.addErrorListener(new BaseErrorListener() {
+      public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol, int line, int charPositionInLine, String msg, RecognitionException e) {
+        throw new ResolveException("Jimple Syntaxerror: " + msg, fileUri, new de.upb.swt.soot.core.model.Position(line, charPositionInLine, -1, -1));
+      }
+    });
+    return parser;
   }
 
 
   @Override
   public void didOpen(DidOpenTextDocumentParams params) {
+    if( params == null ){
+      return;
+    }
+
     super.didOpen(params);
 
     // calculate and cache positions
-    docPosResolver.put(params.getTextDocument().getUri(), new SignaturePositionResolver(params.getTextDocument().getUri(), params.getTextDocument().getText()));
+    docSignaturePositionResolver.put(params.getTextDocument().getUri(), new SignaturePositionResolver(params.getTextDocument().getUri(), params.getTextDocument().getText()));
+
   }
 
   @Override
   public void didClose(DidCloseTextDocumentParams params) {
+    if( params == null ){
+      return;
+    }
+
     super.didClose(params);
     // clear position cache
-    docPosResolver.remove(params.getTextDocument().getUri());
+    docSignaturePositionResolver.remove(params.getTextDocument().getUri());
   }
 
   @Override
@@ -67,9 +100,13 @@ public class JimpleTextDocumentService extends MagpieTextDocumentService {
 
   @Override
   public CompletableFuture<Either<List<? extends Location>, List<? extends LocationLink>>> definition(TextDocumentPositionParams position) {
+    if( position == null ){
+      return null;
+    }
+
     //  go to declaration of Local/Field- if position resolves to a Type -> go to typeDefinition
     return getServer().pool(() -> {
-      final Signature sig = docPosResolver.get(position.getTextDocument().getUri()).resolve(position.getPosition());
+      final Signature sig = docSignaturePositionResolver.get(position.getTextDocument().getUri()).resolve(position.getPosition());
       if (sig == null) {
         // here is nothing to resolve
         return null;
@@ -109,6 +146,9 @@ public class JimpleTextDocumentService extends MagpieTextDocumentService {
 
   @Override
   public CompletableFuture<Either<List<? extends Location>, List<? extends LocationLink>>> typeDefinition(TextDocumentPositionParams position) {
+    if( position == null ){
+      return null;
+    }
     // TODO: resolve position to: Variable/FieldSignature/MethodSignature/TypeSignature - get its type -> return its definition position
     return getServer().pool(() -> {
       return null;
@@ -117,6 +157,9 @@ public class JimpleTextDocumentService extends MagpieTextDocumentService {
 
   @Override
   public CompletableFuture<Either<List<? extends Location>, List<? extends LocationLink>>> declaration(TextDocumentPositionParams params) {
+    if( params == null ){
+      return null;
+    }
     // TODO: find local declaration
     return getServer().pool(() -> {
       String fileUri = params.getTextDocument().getUri();
@@ -124,30 +167,24 @@ public class JimpleTextDocumentService extends MagpieTextDocumentService {
       // TODO: getClass - getMethods; find Method surrounding
       params.getPosition();
       // TODO: parse usages of local in this method -> return declarations ; class: LocalDeclarationResolver
-      /*
-      try {
-        JimpleLexer lexer = new JimpleLexer(CharStreams.fromPath(Paths.get(fileUri)));
-        TokenStream tokens = new CommonTokenStream(lexer);
-        JimpleParser parser = new JimpleParser(tokens);
-        parser.removeErrorListeners();
-        parser.addErrorListener(new BaseErrorListener() {
-          public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol, int line, int charPositionInLine, String msg, RecognitionException e) {
-            throw new ResolveException("Jimple Syntaxerror: " + msg, fileUri, new de.upb.swt.soot.core.model.Position(line, charPositionInLine, -1, -1));
-          }
-        });
 
-        final JimpleDocumentPositionResolver.SignatureOccurenceAggregator signatureOccurenceAggregator = new JimpleDocumentPositionResolver.SignatureOccurenceAggregator();
-        parser.file().enterRule(signatureOccurenceAggregator);
+      try {
+        JimpleParser parser = createParserForUri(fileUri);
+//        parser.file().enterRule(signatureOccurenceAggregator);
 
       } catch (IOException exception) {
         exception.printStackTrace();
-      }*/
+      }
+
       return null;
     });
   }
 
   @Override
   public CompletableFuture<Either<List<? extends Location>, List<? extends LocationLink>>> implementation(TextDocumentPositionParams position) {
+    if( position == null ){
+      return null;
+    }
     // TODO implement: resolve position to MethodSignature/Type and retrieve respective subclasses
     return getServer().pool(() -> {
       return null;
@@ -156,29 +193,98 @@ public class JimpleTextDocumentService extends MagpieTextDocumentService {
 
   @Override
   public CompletableFuture<List<? extends Location>> references(ReferenceParams params) {
-    // TODO: find usages of methods|ClassType|(Locals?)
-    return getServer().pool(() -> {
+    if( params == null ){
       return null;
+    }
+    // find usages of Methods|Classes
+    return getServer().pool(() -> {
+      List<Location> list = new ArrayList<>();
+      final SignaturePositionResolver resolver = docSignaturePositionResolver.get(params.getTextDocument().getUri());
+      final Signature sig = resolver.resolve(params.getPosition());
+      if(sig == null ){
+        return null;
+      }
+
+      boolean includeDef = params.getContext() != null && params.getContext().isIncludeDeclaration();
+      if( sig instanceof ClassType){
+        Optional<? extends AbstractClass<? extends AbstractClassSource>> aClass = getServer().getView().getClass(((ClassType) sig));
+        if(aClass.isPresent()){
+          SootClass sc = (SootClass) aClass.get();
+          list.add(new Location(Util.classToUri(sc), Util.positionToRange(sc.getPosition())));
+        }
+      }else if( sig instanceof MethodSignature){
+        Optional<? extends AbstractClass<? extends AbstractClassSource>> aClass = getServer().getView().getClass(((MethodSignature) sig).getDeclClassType());
+        if(aClass.isPresent()){
+          SootClass sc = (SootClass) aClass.get();
+          final Optional<? extends Method> method = sc.getMethod(((MethodSignature) sig));
+          method.ifPresent(value -> list.add(new Location(Util.classToUri(sc), Util.positionToRange(((SootMethod) value).getPosition()))));
+        }
+      }else if( includeDef ) {
+        if (sig instanceof FieldSignature) {
+          Optional<? extends AbstractClass<? extends AbstractClassSource>> aClass = getServer().getView().getClass(((FieldSignature) sig).getDeclClassType());
+          if (aClass.isPresent()) {
+            SootClass sc = (SootClass) aClass.get();
+            final Optional<? extends Field> field = sc.getField(((FieldSignature) sig));
+            field.ifPresent(value -> list.add(new Location(Util.classToUri(sc), Util.positionToRange(((SootField) value).getPosition()))));
+          }
+        }
+      }
+
+      final Collection<SootClass> classes = (Collection<SootClass>) getServer().getView().getClasses();
+      if(sig instanceof ClassType) {
+        for (SootClass sc : classes) {
+          for (SootMethod method : sc.getMethods()) {
+            final Body body = method.getBody();
+            for (Stmt stmt : body.getStmtGraph().nodes()) {
+              for (Value usesAndDef : stmt.getUsesAndDefs()) {
+                if ( usesAndDef.getType() == sig ) {
+                  list.add(new Location(Util.classToUri(sc), Util.positionToRange(stmt.getPositionInfo().getStmtPosition())));
+                }
+              }
+            }
+          }
+        }
+      }else  if(sig instanceof FieldSignature) {
+        for (SootClass sc : classes) {
+          for (SootMethod method : sc.getMethods()) {
+            final Body body = method.getBody();
+            for (Stmt stmt : body.getStmtGraph().nodes()) {
+              for (Value usesAndDef : stmt.getUsesAndDefs()) {
+                if (usesAndDef instanceof JFieldRef && ((JFieldRef) usesAndDef).getFieldSignature().equals(sig)) {
+                  list.add(new Location(Util.classToUri(sc), Util.positionToRange(stmt.getPositionInfo().getStmtPosition())));
+                }
+              }
+            }
+          }
+        }
+      }else if(sig instanceof MethodSignature){
+        for (SootClass sc : classes) {
+          for (SootMethod method : sc.getMethods()) {
+            final Body body = method.getBody();
+            for (Stmt stmt : body.getStmtGraph().nodes()) {
+              if(stmt instanceof JInvokeStmt || ( stmt instanceof JAssignStmt && ((JAssignStmt) stmt).getRightOp() instanceof AbstractInvokeExpr) ) {
+                list.add(new Location(Util.classToUri(sc), Util.positionToRange(stmt.getPositionInfo().getStmtPosition())));
+              }
+            }
+          }
+        }
+      }
+      return list;
     });
   }
 
 
   @Override
   public CompletableFuture<List<DocumentLink>> documentLink(DocumentLinkParams params) {
+    if( params == null ){
+      return null;
+    }
     // make labels clickable to get to the label
     return getServer().pool(() -> {
 
       try {
         final String fileUri = params.getTextDocument().getUri();
-        JimpleLexer lexer = new JimpleLexer(CharStreams.fromPath(Paths.get(fileUri)));
-        TokenStream tokens = new CommonTokenStream(lexer);
-        JimpleParser parser = new JimpleParser(tokens);
-        parser.removeErrorListeners();
-        parser.addErrorListener(new BaseErrorListener() {
-          public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol, int line, int charPositionInLine, String msg, RecognitionException e) {
-            throw new ResolveException("Jimple Syntaxerror: " + msg, fileUri, new de.upb.swt.soot.core.model.Position(line, charPositionInLine, -1, -1));
-          }
-        });
+        JimpleParser parser = createParserForUri(fileUri);
 
         final JimpleLabelLinkProvider listener = new JimpleLabelLinkProvider();
         parser.file().enterRule(listener);
@@ -195,6 +301,10 @@ public class JimpleTextDocumentService extends MagpieTextDocumentService {
 /*
   @Override
   public CompletableFuture<TypeHierarchyItem> resolveTypeHierarchy(ResolveTypeHierarchyItemParams params) {
+      if( params == null ){
+      return null;
+    }
+
     // TODO:
     return getServer().pool(() -> {
       params.getDirection();
@@ -206,6 +316,10 @@ public class JimpleTextDocumentService extends MagpieTextDocumentService {
 
   @Override
   public CompletableFuture<TypeHierarchyItem> typeHierarchy(TypeHierarchyParams params) {
+      if( params == null ){
+      return null;
+    }
+
     // TODO:
     return getServer().pool(() -> {
       params.getDirection();
@@ -216,6 +330,10 @@ public class JimpleTextDocumentService extends MagpieTextDocumentService {
 
   @Override
   public CompletableFuture<List<? extends DocumentHighlight>> documentHighlight(TextDocumentPositionParams position) {
+      if( params == null ){
+      return null;
+    }
+
     // TODO: implement it for local usage
     return getServer().pool(() -> {
       position.getTextDocument().getUri();
@@ -227,6 +345,10 @@ public class JimpleTextDocumentService extends MagpieTextDocumentService {
 
   @Override
   public CompletableFuture<List<FoldingRange>> foldingRange(FoldingRangeRequestParams params) {
+    if( params == null ){
+      return null;
+    }
+
     return getServer().pool(() -> {
 
       final Optional<? extends AbstractClass<? extends AbstractClassSource>> aClass = getServer().getView().getClass(Util.uritoClasstype(params.getTextDocument().getUri()));
@@ -250,6 +372,10 @@ public class JimpleTextDocumentService extends MagpieTextDocumentService {
 
   @Override
   public CompletableFuture<List<Either<SymbolInformation, DocumentSymbol>>> documentSymbol(DocumentSymbolParams params) {
+    if( params == null ){
+      return null;
+    }
+
     return getServer().pool(() -> {
       final TextDocumentClientCapabilities textDocument = getServer().getClientCapabilities().getTextDocument();
       if (textDocument == null) {
