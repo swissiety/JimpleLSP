e3e4edf8fb09b09b69cb220595f0f40ea61d92c7
Markus Schmidt <markus262@web.de>
2020-11-09 15:59:34 +0100

    fix insertion calc

--- src/main/java/magpiebridge/jimplelsp/resolver/SmartDatastructure.java
+++ src/main/java/magpiebridge/jimplelsp/resolver/SmartDatastructure.java
@@ -12,8 +12,6 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
-
-
 // TODO: improve ds: implement or use dependency for sth like segment/intervaltree
 class SmartDatastructure {
 
@@ -25,11 +23,16 @@ class SmartDatastructure {
   Comparator<Position> comparator = new PositionComparator();
 
   void add(Token start, Token end, Signature sig, @Nullable String identifier) {
+    // insert sorted to be accessed via binary search
     final Position startPos = new Position(start.getLine(), start.getCharPositionInLine());
     int idx = Collections.binarySearch(startPositions, startPos, new PositionComparator());
     if(idx < 0){
-      idx = 0;
+      // calculate insertion index
+      idx = -idx-1;
+    }else{
+      throw new IllegalStateException("position "+startPos+" is already taken.");
     }
+
     startPositions.add(idx, startPos);
     endPositions.add(idx, new Position(end.getLine(), end.getCharPositionInLine()));
     signaturesAndIdentifiers.add(idx, Pair.of(sig, identifier));
@@ -40,11 +43,10 @@ class SmartDatastructure {
     int index = Collections.binarySearch(startPositions, position, new PositionComparator());
 
     if (index < 0 || index >= startPositions.size()) {
-      // not exactly found;
-      // index = -index + 1;
+      // not found
       return null;
     }
-    // FIXME: BUG
+
     if (comparator.compare(startPositions.get(index), position) <= 0
         && comparator.compare(position, endPositions.get(index)) <= 0) {
       return signaturesAndIdentifiers.get(index);
