a01d58230a08fe783d5f44082d1cee1506b33f7c
Markus Schmidt <markus262@web.de>
2021-04-07 19:00:54 +0200

    Revert "remove printed Optional[ ... ] in hover"
    This reverts commit a0a74d39

--- src/main/java/magpiebridge/jimplelsp/JimpleTextDocumentService.java
+++ src/main/java/magpiebridge/jimplelsp/JimpleTextDocumentService.java
@@ -503,9 +503,8 @@ public class JimpleTextDocumentService extends MagpieTextDocumentService {
                 if (aClass.isPresent()) {
                   SootClass sc = (SootClass) aClass.get();
                   str = Modifier.toString(sc.getModifiers()) + " " + sc.toString();
-                  final Optional<ClassType> superclass = sc.getSuperclass();
-                  if (superclass.isPresent()) {
-                    str += "\n extends " + superclass.get().toString();
+                  if (sc.hasSuperclass()) {
+                    str += "\n extends " + sc.getSuperclass();
                   }
 
                   Iterator<ClassType> interfaceIt = sc.getInterfaces().iterator();
